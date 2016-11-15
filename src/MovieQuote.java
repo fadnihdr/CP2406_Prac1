52df18d46f8ff3aaf63a584343b2dc70df0eaece
Fadni Harisyam <jc349598@ad.jcu.edu.au>
2016-11-15 14:25:11 +0800

    Initial commit

---
+++ src/MovieQuote.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jc349598 on 15/11/16.
+ */
+public class MovieQuote {
+    public static void main(String[] args)
+    {
+        System.out.println("The End");
+    }
+
+}
---
+++ src/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jc349598 on 15/11/16.
+ */
+public class MovieQuoteInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("Movie:      Train My Dragons");
+        System.out.println("Character:  Mr.Narrator");
+        System.out.println("Year:       2016");
+
+    }
+
+}
